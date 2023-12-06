199ed032d8d9602e7ebcea59b07f38fe409f8cca
phkane73 <phkane732002@gmail.com>
2023-12-06 14:30:37 +0700

    test-render

---
+++ src/main/java/com/test/render/demo/RenderhostApplication.java
@@ -0,0 +1,13 @@
+package com.test.render.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderhostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderhostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/render/demo/RenderhostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.render.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderhostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
